ee4fbb052d17a098b5ed8912c4887827d2ad729f
vanya <vanya20074@gmail.com>
2015-03-05 21:11:51 +0200

    fix long use freez

--- src/main/java/CapturePhoto.java
+++ src/main/java/CapturePhoto.java
@@ -9,11 +9,12 @@ import java.io.IOException;
 
 public class CapturePhoto implements Runnable {
 
-    private static final int PHOTOS_COUNT = 100;
+
+    private static final int PHOTOS_COUNT = 1000;
     private static final String PHOTOS_DIR = "photos";
-    private static final String OUT_GIF_FILE = "test5.gif";
+    private static final String OUT_GIF_FILE = "lonftime.gif";
     private static final int CAPTURE_DELAY = 10000;
-    private static final int SLIDE_DELAY = 200;
+    private static final int SLIDE_DELAY = 50;
 
     public static void main(String[] args) throws InterruptedException, IOException {
         Runnable r = new CapturePhoto();
@@ -26,32 +27,33 @@ public class CapturePhoto implements Runnable {
     @Override
     public void run() {
         Webcam webcam = Webcam.getDefault();
-        webcam.setViewSize(new Dimension(640, 480));
+      //  webcam.setViewSize(new Dimension(640, 480));
+        webcam.setViewSize(new Dimension(320, 240));
         AnimatedGifEncoder enc = new AnimatedGifEncoder();
         enc.setRepeat(0);
+        /**
+         * 10 - default
+         * 1 - max
+         */
+        enc.setQuality(20);
         enc.start(PHOTOS_DIR + "/" + OUT_GIF_FILE);
         enc.setDelay(SLIDE_DELAY);
         BufferedImage image = null;
-
+        webcam.open();
         for (int i = 0; i < PHOTOS_COUNT; i++) {
-            webcam.open();
+
             try {
                 image = webcam.getImage();
                 ImageIO.write(image, "PNG", new File(PHOTOS_DIR + "/img/photo" + i + ".png"));
                 System.out.println("photos/img/photo" + i + ".png was made");
-
+                Thread.sleep(CAPTURE_DELAY);
             } catch (Exception e) {
                 e.printStackTrace();
             }
             enc.addFrame(image);
-            webcam.close();
-            try {
-                Thread.sleep(CAPTURE_DELAY);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
 
+        }
+        webcam.close();
         if (enc.finish())System.out.println("Gif animation was created");
     }
 }
